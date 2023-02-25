8ae1947d6bf037e8a911312d9c338040d1f9ea8e
Michel Grootjans <michel.grootjans@gmail.com>
2023-02-25 16:53:04 +0100

    cleanup

--- Recycle.WebAPI/Messages/Events.cs
+++ Recycle.WebAPI/Messages/Events.cs
@@ -10,17 +10,7 @@ public record Event
 
 public record Event<TPayload> : Event
 {
-    private TPayload payload;
-
-    [JsonPropertyName("payload")]
-    public TPayload Payload
-    {
-        get => payload;
-        set
-        {
-            payload = value;
-        }
-    }
+    [JsonPropertyName("payload")] public TPayload Payload { get; set; }
 }
 
 public record IdCardRegistered
